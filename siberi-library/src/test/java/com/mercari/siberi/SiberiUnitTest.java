f0bb291bbc7e305a6850b7485acdb7a846d959a2
tomoima525 <tomoima525@gmail.com>
2017-03-06 00:19:58 -0800

    fix test

--- siberi-library/src/test/java/com/mercari/siberi/SiberiUnitTest.java
+++ siberi-library/src/test/java/com/mercari/siberi/SiberiUnitTest.java
@@ -11,7 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
@@ -21,7 +21,7 @@ import static junit.framework.Assert.assertFalse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.KITKAT)
 public class SiberiUnitTest {
 
